96a6c773b4185effaf53333a91623f77b737f6eb
Brainboard <no-reply@brainboard.co>
2023-03-13 11:02:42 +0000

    Brainboard update

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,6 @@
 # All variables as it would be defined in the .tfvars file.
 
+tags = {
+  archuuid = "ac61dd1f-c2ca-4b18-b079-bb9741fd4a08"
+  env      = "Test"
+}
--- variables.tf
+++ variables.tf
@@ -1,9 +1,5 @@
 variable "tags" {
   description = "Default tags to apply to all resources."
   type        = map(any)
-  default = {
-    archuuid = "b30a9bdb-4d19-41ca-a134-dce4d726dd57"
-    env      = "Development"
-  }
 }
 
