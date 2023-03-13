dce0f1fc5421db316119dc5faea1198e3c69a97f
Brainboard <no-reply@brainboard.co>
2023-03-13 10:52:19 +0000

    Brainboard update

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "tags" {
   description = "Default tags to apply to all resources."
   type        = map(any)
   default = {
-    archuuid = "1c84e587-6b90-427d-be87-60f31d8ecb78"
+    archuuid = "b30a9bdb-4d19-41ca-a134-dce4d726dd57"
     env      = "Development"
   }
 }
